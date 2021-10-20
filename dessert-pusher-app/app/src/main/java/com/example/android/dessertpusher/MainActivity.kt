e622b82b954cbafc259c1db447795c681748adfd
Andrew Weiss <andreivayss@gmail.com>
2021-10-20 11:33:09 +0700

    Added lifecycle observer

--- dessert-pusher-app/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
+++ dessert-pusher-app/app/src/main/java/com/example/android/dessertpusher/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     private var revenue = 0
     private var dessertsSold = 0
+    private lateinit var dessertTimer: DessertTimer
 
     // Contains all the views
     private lateinit var binding: ActivityMainBinding
@@ -75,6 +76,8 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
             onDessertClicked()
         }
 
+        dessertTimer = DessertTimer(this.lifecycle)
+
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -85,6 +88,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStart() {
         super.onStart()
+        //dessertTimer.startTimer()
         Timber.i("onStart called")
     }
 
@@ -105,6 +109,7 @@ class MainActivity : AppCompatActivity(), LifecycleObserver {
 
     override fun onStop() {
         super.onStop()
+        //dessertTimer.stopTimer()
         Timber.i("onStop called")
     }
 
